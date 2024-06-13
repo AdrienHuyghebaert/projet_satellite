aaaef287f4a32eec288cdc81e92f9a202ca3f9a1
Adrien Huyghebaert <adrien.huyghebaert.1@ens.etsmtl.ca>
2024-06-13 16:08:19 -0400

    Ajout de la fonction get_user_input.py et test dans le main

---
+++ SatelliteObservation/ExtraFunctions/get_user_input.py
@@ -0,0 +1,47 @@
+# Ce script contient plusieurs fonctions permettant de s'assurer
+# que le format de donnée que l'utilisateur rentre est le bon
+
+def get_float_input(prompt):
+    while True:
+        # Constantly check if a value was entered followed by Enter
+        try:
+            # Put the value entered by user in "entree"
+            entree = input(prompt)
+            # Try to transform it into a float, if it cannot, we go to "except"
+            value = float(entree)
+            break
+        except ValueError:
+            # If input is not a float we get a ValueError
+            print("Invalid input, please try again.")
+    return value
+
+
+
+def get_int_input(prompt):
+    while True:
+        # Constantly check if a value was entered followed by Enter
+        try:
+            # Put the value entered by user in "entree"
+            entree = input(prompt)
+            # Try to transform it into an int, if it cannot, we go to "except"
+            value = int(entree)
+            break
+        except ValueError:
+            # If input is not a float we get a ValueError
+            print("Invalid input, please try again.")
+    return value
+
+
+def get_str_input(prompt):
+    while True:
+        # Constantly check if a value was entered followed by Enter
+        try:
+            # Put the value entered by user in "entree"
+            entree = input(prompt)
+            # Try to transform it into a str, if it cannot, we go to "except"
+            value = str(entree)
+            break
+        except ValueError:
+            # If input is not a float we get a ValueError
+            print("Invalid input, please try again.")
+    return value
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import SatelliteObservation
+
+float = SatelliteObservation.get_float_input('Entrez un float : \n')
+print(float)
+entier = SatelliteObservation.get_int_input('Entrez un entier : \n')
+print(entier)
+str = SatelliteObservation.get_str_input('Entrez un str : \n')
+print(str)
